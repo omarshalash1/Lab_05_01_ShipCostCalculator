8eaebc58105f79a101b0312bf17de9950dc722be
omars <omars@DESKTOP-PURJVSK.lan>
2023-02-10 14:44:51 -0500

    ShipCostCalculator

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      System.out.print("Enter the price of the item: $");
+      double itemPrice = 700;
+      double shippingCost;
+      if (itemPrice >= 100) {
+         shippingCost = 0;
+      } else {
+         shippingCost = itemPrice * 0.02;
+      }
+      double totalPrice = itemPrice + shippingCost;
+      System.out.println("Shipping cost: $" + shippingCost);
+      System.out.println("Total price: $" + totalPrice);
+   }
+}
