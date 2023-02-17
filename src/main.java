3d7d9a0f2d930db0903b9dc13f2ff0f1e3810b6d
omarshalash1 <124627562+omarshalash1@users.noreply.github.com>
2023-02-17 10:54:36 -0500

    Update main.java
    comments added to the code

--- src/main.java
+++ src/main.java
@@ -1,18 +1,34 @@
+// This code imports the Scanner class from the java.util package.
 import java.util.Scanner;
-
+// This line declares a public class called "main".
 public class main {
-   public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      System.out.print("Enter the price of the item: $");
-      double itemPrice = 100;
-      double shippingCost;
-      if (itemPrice >= 100) {
-         shippingCost = 0;
-      } else {
-         shippingCost = itemPrice * 0.02;
-      }
-      double totalPrice = itemPrice + shippingCost;
-      System.out.println("Shipping cost: $" + shippingCost);
-      System.out.println("Total price: $" + totalPrice);
-   }
+// This line declares a static void method called "main" that takes in an array of Strings as its argument.
+public static void main(String[] args) {
+// This line creates a new Scanner object called "scan".
+Scanner scan = new Scanner(System.in);
+     // This line prints a message asking the user to enter the price of an item.
+  System.out.print("Enter the price of the item: $");
+  
+  // This line initializes a double variable called "itemPrice" to 100. (Note: this value is hard-coded and not based on user input.)
+  double itemPrice = 100;
+  
+  // This line initializes a double variable called "shippingCost".
+  double shippingCost;
+  
+  // This line begins an if-else statement. If the itemPrice is greater than or equal to 100, the shippingCost is set to 0. Otherwise, the shipping cost is calculated as 2% of the itemPrice.
+  if (itemPrice >= 100) {
+     shippingCost = 0;
+  } else {
+     shippingCost = itemPrice * 0.02;
+  }
+  
+  // This line calculates the total price of the item (itemPrice plus shippingCost) and initializes a double variable called "totalPrice" to that value.
+  double totalPrice = itemPrice + shippingCost;
+  
+  // This line prints the shippingCost with a message.
+  System.out.println("Shipping cost: $" + shippingCost);
+  
+  // This line prints the totalPrice with a message.
+  System.out.println("Total price: $" + totalPrice);
+}
 }
